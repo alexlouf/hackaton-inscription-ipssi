56f0af37af52c10021a47f432a72c6acae5c09d4
Alexandre <contact@alexandrelouf.fr>
2019-12-19 11:36:50 -0800

    add-get-request and auto refresh

--- code/script.js
+++ code/script.js
@@ -4,9 +4,9 @@ window.addEventListener("DOMContentLoaded", ()=>{
     // retrieving DOM elements necessary for developping
     submitButton = document.getElementById("submit-form");
     form = document.getElementById("form");
+    array = document.getElementById("registered-users")
 
-    console.log(submitButton);
-
+    //Adding actions to DOM
     submitButton.onclick = function(e){
 
         e.preventDefault();
@@ -35,8 +35,17 @@ window.addEventListener("DOMContentLoaded", ()=>{
             alert(message);
             return;
         }
+
+        fetchArray(array);
+
         return;
     }
+
+    //Setting intervals to refresh registered student
+    fetchArray(array);
+    refreshArray = setInterval(() => {
+        fetchArray(array);
+    }, 30000);
 
 });
 
@@ -59,4 +68,61 @@ function controlText(text) {
     // return true ou flase si c'est bon ou pas
 
     return true;
+}
+
+function fetchArray(array) {
+    console.log(array)
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+        editArray(this.responseText, array);
+    }
+    });
+
+    xhr.open("GET", "local-api.php");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send();
+
+}
+
+function editArray(data, array){
+    json = JSON.parse(data);
+    tbody = array.children[1];
+    tbody.innerHTML = "";
+    //tr = document.createElement("tr");
+    //thName = document.createElement("td");
+    //thSurname = document.createElement("td");
+    //thEmail = document.createElement("td");
+    //thPromotion = document.createElement("td");
+    //thSpeciality = document.createElement("td");
+
+    for (var i = 0; i < json.length; i++) {
+        //creating dom elements
+        tr = document.createElement("tr");
+        thName = document.createElement("td");
+        thSurname = document.createElement("td");
+        thEmail = document.createElement("td");
+        thPromotion = document.createElement("td");
+        thSpeciality = document.createElement("td");
+
+        //Add values to dom elements
+        thName.innerHTML = json[0].name;
+        thSurname.innerHTML = json[0].surname;
+        thEmail.innerHTML = json[0].email;
+        thPromotion.innerHTML = json[0].promotion;
+        thSpeciality.innerHTML = json[0].speciality
+
+        //adding to HTML
+        tr.appendChild(thName);
+        tr.appendChild(thSurname);
+        tr.appendChild(thEmail);
+        tr.appendChild(thPromotion);
+        tr.appendChild(thSpeciality);
+        tbody.appendChild(tr);
+    }
+
 }
